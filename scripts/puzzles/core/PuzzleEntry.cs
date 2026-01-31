6e9b412a9867fa8a9766ae4247becb311a943ce6
Ksawery <cc241042@fhstp.ac.at>
2026-01-31 16:03:06 +0100

    puzzleHost now support multiple puzzles

---
+++ scripts/puzzles/core/PuzzleEntry.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace GGJ_2026.scripts.puzzles.core
+{
+    [GlobalClass]
+    public partial class PuzzleEntry : Resource
+    {
+        [Export] public string Id = "";
+        [Export] public PackedScene Scene;
+    }
+}
