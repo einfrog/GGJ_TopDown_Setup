c4b8de21ee4021138948573c32ed1f5bf0178aa3
cc241051 <cc241051@fhstp.ac.at>
2026-01-31 19:39:57 +0100

    Health bar with decrease

---
+++ scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using GGJ_2026.scripts;
+using Godot;
+
+public partial class HealthBar : Control
+{
+    [Export]
+    public TextureProgressBar Bar;
+
+    public override void _Ready()
+    {
+
+        if (Player.Instance != null)
+        {
+            Bar.MaxValue = Player.Instance.MaxHealth;
+            Bar.Value = Player.Instance.Health;
+
+            Player.Instance.HealthChanged += UpdateHealth;
+        }
+    }
+
+    public void UpdateHealth(float currentHealth)
+    {
+        Bar.Value = currentHealth;
+    }
+    
+    public override void _ExitTree()
+    {
+        if (Player.Instance != null)
+            Player.Instance.HealthChanged -= UpdateHealth;
+    }
+}
