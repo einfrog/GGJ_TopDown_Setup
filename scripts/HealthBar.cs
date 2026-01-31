b61b71625d747c2a5821737edcdd2d513aa0e4b7
Zottel Emilio <e.zottel@pke.at>
2026-01-31 20:24:54 +0100

    Fix health bar

--- scripts/HealthBar.cs
+++ scripts/HealthBar.cs
@@ -1,31 +1,28 @@
-﻿using GGJ_2026.scripts;
-using Godot;
+﻿using Godot;
+
+namespace GGJ_2026.scripts;
 
 public partial class HealthBar : Control
 {
+
     [Export]
     public TextureProgressBar Bar;
 
     public override void _Ready()
     {
-
-        if (Player.Instance != null)
-        {
-            Bar.MaxValue = Player.Instance.MaxHealth;
-            Bar.Value = Player.Instance.Health;
-
-            Player.Instance.HealthChanged += UpdateHealth;
-        }
+        Bar.MaxValue = Player.Instance.MaxHealth;
+        Bar.Value = Player.Instance.Health;
+        Player.Instance.HealthChanged += UpdateHealth;
     }
 
     public void UpdateHealth(float currentHealth)
     {
         Bar.Value = currentHealth;
     }
-    
+
     public override void _ExitTree()
     {
-        if (Player.Instance != null)
-            Player.Instance.HealthChanged -= UpdateHealth;
+        Player.Instance.HealthChanged -= UpdateHealth;
     }
+
 }
